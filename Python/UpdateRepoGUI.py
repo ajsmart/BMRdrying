e1528bc88b55470ce40e2a76ded1aa0b76bbb677
ajsmart <alex@smarts.us>
2018-01-08 10:12:11 -0700

    automated python git update1-8-2018 10:12

--- Python/UpdateRepoGUI.py
+++ Python/UpdateRepoGUI.py
@@ -1,7 +1,36 @@
 import subprocess
+import datetime
+from appJar import gui
+import os
 
-if __name__ == '__main__':
+
+def commit(button):
+    if button == "Exit":
+        app.stop()
     upd8 = "git pull"
     add = "git add --all"
+    now = datetime.datetime.now()
+    year = str(now.year)
+    month = str(now.month)
+    day = str(now.day)
+    hour = str(now.hour)
+    minute = str(now.minute)
+    commit = "git commit -m \"automated python git update" + month + "-" + day + "-" + year + " " + hour + ":" + minute +"\""
+    push = "git push origin master"
+    os.system(upd8)
+    os.system(add)
+    os.system(commit)
+    os.system(push)
+#######################################
+if __name__ == '__main__':
+    app = gui("Selection Window","500x200")
+    app.setBg("PaleTurquoise")
+    app.setFont(12)
 
-    commit = "git commit -m \"automated python git update\""
+    app.addLabel("title", "Push data to GIT repository.")
+    app.setLabelBg("title", "Black")
+    app.setLabelFg("title", "PaleTurquoise")
+
+    app.addButtons(["Push","Exit"], commit)
+
+    app.go()
