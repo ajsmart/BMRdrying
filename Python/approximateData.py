39cd85230a926fe5d2194286e23bb6ad260b8a99
ajsmart <alex@smarts.us>
2018-01-29 12:37:59 -0700

    coding update

--- Python/approximateData.py
+++ Python/approximateData.py
@@ -9,8 +9,21 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     #import the relevant data.
     pdata = pd.read_csv('data/Laser/01.16.18_031_laser_open.csv')
-    sdata = pdata['-999.999'].as_matrix()
-    num = 6 # number of legandre polynomials we will use to model data
+    tmp = pdata['-999.999'].values.tolist()
+    #determine start time...
+    started = False #we have not started the data recording yet
+    mvec = []
+    for x in tmp:
+        if x <-10 and started == False:
+            continue
+        elif x > -10 and started == False:
+            started = True
+        if started == True:
+            mvec.append(x)
+    sdata = np.asarray(mvec)
+
+    #set other paramaters
+    num = 13 # number of legandre polynomials we will use to model data
     len = sdata.size
 
     # generate legandre polynomials through orthogonalization
@@ -22,17 +35,18 @@ if __name__ == '__main__':
     # now to orthogonalize those polynomials
     e = p
     q = np.zeros(s)
-    np1 = np.linalg.norm(p[:,0])
-    q[:,0] = p[:,0]/np1
+    np1 = p[:,0].dot(p[:,0])/np.float(len)
+    q[:,0] = p[:,0]/np.sqrt(np1)
+
     for j in range(1,num):
         for k in range(0,j):
-            e[:,j] = e[:,j] - p[:,j].dot(q[:,k])/np.float(len)*q[:,k]
-        nej = np.linalg.norm(e[:,j])
+            e[:,j] = e[:,j] - (p[:,j].dot(q[:,k])/np.float(len)*q[:,k])
+        nej = np.sqrt(e[:,j].dot(e[:,j])/np.float(len))
         q[:,j] = e[:,j]/nej
     
-    #plt.figure(1)
-    #plt.plot(t,q)
-    #plt.title('Legendre Polynomials')
+    plt.figure(1)
+    plt.plot(t,q)
+    plt.title('Legendre Polynomials')
     #plt.show()
     
     # now lets get the approximation
@@ -45,8 +59,10 @@ if __name__ == '__main__':
         for j in range(0,num):
             g[i,j] = q[:,i].dot(q[:,j])/np.float(len)
 
-    c = (g**-1).dot(a)
+    c = np.linalg.inv(g).dot(a)
+    approx = (c.conj().T.dot(q.conj().T)).conj().T
+    
     plt.figure(2)
-    plt.plot(t,sdata,'b',t,(c.conj().T.dot(q.conj().T)).conj().T,'r')
+    plt.plot(t,sdata,'b',t,approx,'r')
     plt.title('Legandre Approximation')
     plt.show()
